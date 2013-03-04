4d6ad53a1f1b3c6cce9d578c392b621c7a743445
xuyue <xuyue@node01.(none)>
2013-03-04 17:39:09 +0800

    File CODING modified.

---
+++ src/masternode/masternode_main.cc
@@ -0,0 +1,23 @@
+//-------------------------------------------------------------
+// Created: 2013/3/4
+// Author: Leo_xy
+// Last modified: 2013/3/4
+// Version: 0.1
+// File: src/masternode/masternode_main.cc
+// Breif: Driver code that start masternode server.
+// -----------------------------------------------------------
+
+namespace filecached
+{
+	class MasterNode
+	{
+	}
+}
+
+int
+main(int argc, char **argv)
+{
+	filecached::MasterNode::run(argc, argv);
+}
+
+
